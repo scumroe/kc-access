42d6439777f7cccb36af91c0544ed3c7abb7f90b
scumroe <39536260+scumroe@users.noreply.github.com>
2018-09-17 15:11:37 +0100

    Add files via upload

---
+++ challenges/Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("What is your name?")
+        Dim name As String = Console.ReadLine()
+        Console.WriteLine("Hello, {0}", name)
+        Console.Read()
+    End Sub
+
+End Module
